b757c3dea01864442b462c5e22c674903c6f59c8
smalldot <z0976530159@gmail.com>
2022-08-17 21:46:14 +0800

    Update 2022-08-17T13:46:10.322Z

---
+++ assets/js/all.js
@@ -0,0 +1,35 @@
+"use strict";
+
+$(document).ready(function () {
+  //手機搜尋
+  $('#m-search').click(function (event) {
+    event.preventDefault();
+    $('#search-nav').removeClass('hidden');
+    $('#search-nav').addClass('flex');
+    $('#h1').toggleClass('hidden');
+    $('#m-search-list').removeClass('flex');
+    $('#m-search-list').addClass('hidden');
+  });
+  $('#search-pre').click(function (event) {
+    event.preventDefault();
+    $('#search-nav').removeClass('flex');
+    $('#search-nav').addClass('hidden');
+    $('#h1').toggleClass('hidden');
+    $('#m-search-list').removeClass('hidden');
+    $('#m-search-list').addClass('flex');
+  }); //手機選單
+
+  $('#m-menu').click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('hidden');
+    $('#close').toggleClass('hidden');
+    $('#m-menu-list').slideToggle();
+  });
+  $('#close').click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('hidden');
+    $('#m-menu').toggleClass('hidden');
+    $('#m-menu-list').slideToggle();
+  });
+});
+//# sourceMappingURL=all.js.map
